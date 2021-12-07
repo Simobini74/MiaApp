1d4104084f36f997a58629eb17b243738cf9ea0a
Simobini74 <17884614@studenti.unipi.it>
2021-12-07 19:50:45 +0100

    2 files modified

--- src/app/chiave/chiave.component.ts
+++ src/app/chiave/chiave.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-chiave',
   templateUrl: './chiave.component.html',
-  styleUrls: ['./chiave.component.css']
+  styleUrls: ['./chiave.component.css'],
 })
-export class ChiaveComponent implements OnInit {
+export class ChiaveComponent {
+  @Output() newKeyEvent = new EventEmitter<string>();
+  @Output() newAltroEvent = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  useKey(key: string) {
+    this.newKeyEvent.emit(key);
+  }
+  getKey() {
+    this.newAltroEvent.emit();
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
