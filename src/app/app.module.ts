7de8059c39267f47e53778dcba694ab76821c458
Simobini74 <17884614@studenti.unipi.it>
2021-12-07 19:21:58 +0100

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ChiaveComponent } from './chiave/chiave.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent, ChiaveComponent ],
   bootstrap:    [ AppComponent ]
 })
