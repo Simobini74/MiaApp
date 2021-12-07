2a2e9cfa4b4f97fb210d9f09ac4007ea69aa0f93
Simobini74 <17884614@studenti.unipi.it>
2021-12-07 19:55:11 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PostserviceService } from './postservice.service';
+import { HttpClientModule } from '@angular/common/http';
 import { ChiaveComponent } from './chiave/chiave.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ChiaveComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent, ChiaveComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
