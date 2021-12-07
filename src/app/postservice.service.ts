e9b06fe5b69a0fcd5c509af386036c7d7aca7543
Simobini74 <17884614@studenti.unipi.it>
2021-12-07 21:27:10 +0100

    1 file modified

--- src/app/postservice.service.ts
+++ src/app/postservice.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class PostserviceService {
 
+  apiKEY: string = '';
+  apiURL: string =
+    'https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/kvaas-giwjg/service/kvaas/incoming_webhook';
+
   constructor(private service: HttpClient) { }
+
+  public Key() {
+    let promise2 = fetch(this.apiURL + '/new', { method: 'POST' }).then(
+      response => response.json(),
+      error => alert(error)
+    );
+    return promise2;
+  }
 
 }
