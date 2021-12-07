3afc9567a9636c4c762b4df578d24c303dcce1a1
Simobini74 <17884614@studenti.unipi.it>
2021-12-07 21:21:57 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,16 @@ export class AppComponent {
   PostSalvato: Array<NuovoPost> = [];
   nuovo: boolean = false;
 
+  getKey() {
+    this.service.Key().then(key => {
+      fetch(this.service.apiURL + '/post?key=' + key + '&msg=' + {}, {
+        method: 'POST'
+      }).then(response => response.json(), error => alert(error));
+      this.key = key;
+    });
+    this.log = true;
+  }
+
   aggiungi() {
     this.nuovo = true;
   }
