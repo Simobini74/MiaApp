613fe2b9cbeecc63418a8de0ba758bb504654656
Simobini74 <17884614@studenti.unipi.it>
2021-12-07 20:00:40 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { HelloComponent } from './hello.component';
 import { PostserviceService } from './postservice.service';
-
 import { ChiaveComponent } from './chiave/chiave.component';
+
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
