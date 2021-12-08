75f1656a10e060039524925a5c429f0da73d6380
Simobini74 <17884614@studenti.unipi.it>
2021-12-08 09:36:30 +0100

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { HelloComponent } from './hello.component';
 import { ChiaveComponent } from './chiave/chiave.component';
 import { PostserviceService } from './postservice.service';
+import { NuovopostitComponent } from './nuovopostit/nuovopostit.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent, ChiaveComponent],
+  declarations: [AppComponent, HelloComponent, ChiaveComponent, NuovopostitComponent],
   bootstrap: [AppComponent],
   providers: [PostserviceService],
 })
