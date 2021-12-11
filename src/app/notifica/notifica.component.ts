3b5b113120d3f8a0e05b6e66d6900f68f4aca439
Simobini74 <17884614@studenti.unipi.it>
2021-12-11 11:59:08 +0100

    2 files modified, 1 added

--- src/app/notifica/notifica.component.ts
+++ src/app/notifica/notifica.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import {NuovoPost} from '../app.component'
 
-
-
 @Component({
   selector: 'app-notifica',
   templateUrl: './notifica.component.html',
   styleUrls: ['./notifica.component.css']
 })
-export class NotificaComponent  {
+export class NotificaComponent {
 
   @Input() selezioneC: NuovoPost;
+
+  clean() {
+    this.selezioneC.contenuto = undefined;
+   }
 
   constructor() { }
 
